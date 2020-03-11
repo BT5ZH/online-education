f4da17c26496eb100789979932ccbe6012d080e6
BoTingZH <540761800@.com>
2020-03-11 17:14:18 +0800

    增加个人收藏课程页面

---
+++ src/views/online/learning/MyCourses.vue
@@ -0,0 +1,46 @@
+<template>
+    <main class="rs-myCourses">
+        <div class="myCourses__area">
+            <div class="myCourses__area__card">
+                <li class="myCourses__area__card--add" >
+                    <svg>
+                        <use xlink:href="../../../assets/img/all.svg#icon-book"></use>
+                    </svg>
+                </li>
+                <div class="myCourses__area__card--title">
+                    您一共收藏了 XX 门课程
+                </div>
+            </div>
+            <div v-for = "(course,index) in courseList" :key="index" class="myCourses__area__card">
+                <img :src="courseList[index].COURSE_COVERURL" alt="House 3" class="myCourses__area__card--img">
+                <h5 class="myCourses__area__card--name">{{courseList[index].COURSE_STATUS}}</h5>
+                <div class="myCourses__area__card--title">
+                    {{courseList[index].COURSE_NAME}}
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        computed: {
+            courseList(){
+                return this.$store.state.courseShortList;
+            }
+
+        },
+        created() {
+            this.$store.dispatch("getUserCourses").then(() => {
+                console.log("-----------");
+            }).catch((err) => {
+                console.error(err);
+            });
+
+        }
+    }
+</script>
