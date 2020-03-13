cc75f40d9d9344106bba6b25cea732ba58c6348c
BoTingZH <540761800@.com>
2020-03-13 16:25:48 +0800

    初始化 profile

---
+++ src/views/online/profile/Profile.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="rs-profile">
+        <div class="profile__card">
+            <img class="profile__card--view" src="https://rs-learning-resources.s3.cn-northwest-1.amazonaws.com.cn/teaching/resources/2704133E/AVATAR01.jpg" alt="avatar">
+            <div class="profile__card__txt">
+                <div class="profile__card__txt--name">
+                    张汇泉
+                </div>
+                <div class="profile__card__txt--email">
+                    540761800@qq.com
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
